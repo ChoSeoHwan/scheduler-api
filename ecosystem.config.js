66943dc04c63b359e0b98bf6d68d97a4f31f517a
ChoSeoHwan <wtae1216@naver.com>
2022-01-31 18:23:55 +0900

    fix: production/development 환경에 대한 pm2 config file 작성

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,43 @@
-module.exports = {
-    apps: [
-        {
-            name: 'scheduler-api',
-            script: `./dist/main.js`,
-            instances: 0,
-            exec_mode: 'cluster',
-            wait_ready: true,
-            listen_timeout: 30000,
-            kill_timeout: 5000,
-            env: {
-                "NODE_ENV": "development"
-            },
-            env_production: {
-                "NODE_ENV": "production"
-            }
-        }
+let isProduction = false;
+process.argv.forEach((arg, index) => {
+    if (!arg.startsWith('--env')) return true;
 
-    ]
+    const environment = arg.split('=')[1] || process.argv[index + 1];
+    if (environment === 'production') isProduction = true;
+
+    return false;
+});
+
+let schedulerApiApp = {
+    name: 'scheduler-api',
+    script: `./dist/main.js`,
+    exec_mode: 'cluster',
+    wait_ready: true,
+    listen_timeout: 30000,
+    kill_timeout: 5000,
+};
+
+if (isProduction) {
+    schedulerApiApp = {
+        ...schedulerApiApp,
+        instances: 0,
+        env: {
+            NODE_ENV: 'production'
+        }
+    }
+} else {
+    schedulerApiApp = {
+        ...schedulerApiApp,
+        instances: 1,
+        watch: ['dist'],
+        watch_delay: 1000,
+        ignore_watch: ["node_modules"],
+        env: {
+            NODE_ENV: "development"
+        }
+    }
+}
+
+module.exports = {
+    apps: [schedulerApiApp]
 }
