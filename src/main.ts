82af383c073b9ded948f0c0fbf73368e69c4348d
ChoSeoHwan <wtae1216@naver.com>
2022-02-01 15:34:39 +0900

    chore: ci test 를 위한 pr

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from '~/app.module';
 import { ServerConfig } from '~/initialize/configs/server.config';
 
 const bootstrap = async () => {
+    console.log('ci test');
+
     const app = await NestFactory.create(AppModule);
 
     // 각종 라이브러리에 container 주입
