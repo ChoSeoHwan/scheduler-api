44976b64e91e7435d195ecfbecab6a3dd4e18123
ChoSeoHwan <wtae1216@naver.com>
2022-02-01 23:29:49 +0900

    chore: cd 수정

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -3,8 +3,9 @@ SOURCE_ROOT=/ec2-user/scheduler-api
 
 cd $SOURCE_ROOT || exit 1
 
-ls -al
+docker exec node-api ls -al
 
+exit 1;
 # production 모드로 dependency 설치
 docker exec node-api yarn install --production
 
